3867cdca49ba8f3dca91838bcac6d8320a96b2ab
mjb-0001 <56898558+mjb-0001@users.noreply.github.com>
2020-04-09 15:51:42 -0500

    Add files via upload

---
+++ 04-08-2020-word2vec/json-to-dict.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 01:09:36 2020
+
+@author: strik
+"""
+
+import json
+import numpy as np
+
+import pandas as pd
+# data = None
+# with open(r'C:\Users\strik\Desktop\comm_use_subset\pmc_json\finalfile.xml.json') as file:
+#     data = json.load(file)
+    
+
+
+
+
+df = pd.read_csv(r'C:\Users\strik\Desktop\comm_use_subset\CORD-19-research-challenge\metadata.csv')
+df2 = pd.read_csv(r'C:\Users\strik\Desktop\comm_use_subset\CORD-19-research-challenge\metadata.csv', usecols=['abstract'])
+
+df2.to_csv('all-text.csv', header=False, index=False)
+
+
+# data = df2.to_numpy()
